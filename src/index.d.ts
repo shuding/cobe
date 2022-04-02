be4e3e3711889bbd3ab3d3ec8df96731f7e7be27
Shu Ding <g@shud.in>
2022-04-02 16:47:40 +0200

    0.5.0

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,42 +1,30 @@
-import Phenomenon from "phenomenon";
-
-declare namespace OptionKey {
-  const OPT_PHI = "phi";
-  const OPT_THETA = "theta";
-  const OPT_DOTS = "mapSamples";
-  const OPT_MAP_BRIGHTNESS = "mapBrightness";
-  const OPT_BASE_COLOR = "baseColor";
-  const OPT_MARKER_COLOR = "markerColor";
-  const OPT_GLOW_COLOR = "glowColor";
-  const OPT_MARKERS = "markers";
-  const OPT_DIFFUSE = "diffuse";
-  const OPT_DPR = "devicePixelRatio";
-  const OPT_DARK = "dark";
-}
+import Phenomenon from 'phenomenon'
 
 export interface Marker {
-  location: [number, number];
-  size: number;
+  location: [number, number]
+  size: number
 }
 
 export interface COBEOptions {
-  width: number;
-  height: number;
-  onRender: (state: Record<string, any>) => void;
-  [OptionKey.OPT_PHI]: number;
-  [OptionKey.OPT_THETA]: number;
-  [OptionKey.OPT_DOTS]: number;
-  [OptionKey.OPT_MAP_BRIGHTNESS]: number;
-  [OptionKey.OPT_BASE_COLOR]: [number, number, number];
-  [OptionKey.OPT_MARKER_COLOR]: [number, number, number];
-  [OptionKey.OPT_GLOW_COLOR]: [number, number, number];
-  [OptionKey.OPT_MARKERS]: Marker[];
-  [OptionKey.OPT_DIFFUSE]: number;
-  [OptionKey.OPT_DPR]: number;
-  [OptionKey.OPT_DARK]: number;
+  width: number
+  height: number
+  onRender: (state: Record<string, any>) => void
+  phi: number
+  theta: number
+  mapSamples: number
+  mapBrightness: number
+  baseColor: [number, number, number]
+  markerColor: [number, number, number]
+  glowColor: [number, number, number]
+  markers: Marker[]
+  diffuse: number
+  devicePixelRatio: number
+  dark: number
+  offset?: [number, number]
+  scale?: number
 }
 
 export default function createGlobe(
   canvas: HTMLCanvasElement,
   opts: COBEOptions
-): Phenomenon;
+): Phenomenon
