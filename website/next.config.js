094ee06a7e8413e4507957f68767ffe13f43b954
Shu Ding <g@shud.in>
2022-04-23 16:32:37 +0200

    website

---
+++ website/next.config.js
@@ -0,0 +1,8 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+})
+
+module.exports = withNextra({})
---
+++ website/pages/configurations.mdx
@@ -0,0 +1,2 @@
+# Configurations
+
