349fad3ede163e3545b8d323108068d550a3a8d3
Tom Planche <Tomplanche@icloud.com>
2025-03-22 02:33:55 +0100

    [96] (feat on main) Added `color` option for markers

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,33 +1,34 @@
-import Phenomenon from 'phenomenon'
+import type Phenomenon from "phenomenon";
 
 export interface Marker {
-  location: [number, number]
-  size: number
+  location: [number, number];
+  size: number;
+  color?: [number, number, number];
 }
 
 export interface COBEOptions {
-  width: number
-  height: number
-  onRender: (state: Record<string, any>) => void
-  phi: number
-  theta: number
-  mapSamples: number
-  mapBrightness: number
-  mapBaseBrightness?: number
-  baseColor: [number, number, number]
-  markerColor: [number, number, number]
-  glowColor: [number, number, number]
-  markers: Marker[]
-  diffuse: number
-  devicePixelRatio: number
-  dark: number
-  opacity?: number
-  offset?: [number, number]
-  scale?: number
-  context?: WebGLContextAttributes
+  width: number;
+  height: number;
+  onRender: (state: Record<string, any>) => void;
+  phi: number;
+  theta: number;
+  mapSamples: number;
+  mapBrightness: number;
+  mapBaseBrightness?: number;
+  baseColor: [number, number, number];
+  markerColor: [number, number, number];
+  glowColor: [number, number, number];
+  markers: Marker[];
+  diffuse: number;
+  devicePixelRatio: number;
+  dark: number;
+  opacity?: number;
+  offset?: [number, number];
+  scale?: number;
+  context?: WebGLContextAttributes;
 }
 
 export default function createGlobe(
   canvas: HTMLCanvasElement,
-  opts: COBEOptions
-): Phenomenon
+  opts: COBEOptions,
+): Phenomenon;
